5a797fd5a371438d04f59f2c1144e46acb79fc1c
yym <771339512@qq.com>
2024-04-03 18:24:06 +0800

    引入ImGui，并实现基础的ImGuiLayer

--- premake5.lua
+++ premake5.lua
@@ -13,9 +13,11 @@ outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 IncludeDir = {}
 IncludeDir["GLFW"] = "Hiper/vendor/GLFW/include"
 IncludeDir["Glad"] = "Hiper/vendor/Glad/include"
+IncludeDir["ImGui"] = "Hiper/vendor/imgui"
 
 include "Hiper/vendor/GLFW"
 include "Hiper/vendor/Glad"
+include "Hiper/vendor/imgui"
 
 project "Hiper"
 
@@ -40,13 +42,16 @@ project "Hiper"
 		"%{prj.name}/src",
 		"%{prj.name}/vendor/spdlog/include",
 		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}"
+		"%{IncludeDir.Glad}",
+		"%{IncludeDir.ImGui}"
+
 	}
 
 	links 
 	{ 
 		"GLFW",
 		"Glad",
+		"ImGui",
 		"opengl32.lib"
 	}
 
