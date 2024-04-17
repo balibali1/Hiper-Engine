a3db8672d420eee20d4b414c4543cf11fe51c09a
yym <771339512@qq.com>
2024-04-17 14:18:04 +0800

    更新项目配置文件和Debug宏定义

--- Hiper/src/Hiper/Core.h
+++ Hiper/src/Hiper/Core.h
@@ -10,6 +10,10 @@
 	#error Hiper only support Windows!
 #endif // HP_PLATFORM_WINDOWS
 
+#ifdef HP_DEBUG
+	#define HP_ENABLE_ASSERTS
+#endif
+
 #ifdef HP_ENABLE_ASSERTS
 	#define HP_ASSERT(x, ...) { if(!(x)) { HP_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
 	#define HP_CORE_ASSERT(x, ...) { if(!(x)) { HP_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
--- premake5.lua
+++ premake5.lua
@@ -1,5 +1,7 @@
 workspace "Hiper"
 	architecture "x64"
+	startproject "Sandbox"
+
 	configurations
 	{
 		"Debug",
@@ -24,6 +26,7 @@ project "Hiper"
 	location "Hiper"
 	kind "SharedLib"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -57,31 +60,33 @@ project "Hiper"
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
+
 		defines
 		{
 			"HP_PLATFORM_WINDOWS",
-			"HP_BUILD_DLL"
+			"HP_BUILD_DLL",
+			"GLFW_INCLUDE_NONE"
 		}
+
 		postbuildcommands
 		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+			("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
 		}
 
 	filter "configurations:Debug"
 		defines "HP_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HP_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HP_DIST"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 project "Sandbox"
@@ -89,6 +94,7 @@ project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
 	language "C++"
+	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -112,8 +118,8 @@ project "Sandbox"
 
 	filter "system:windows"
 		cppdialect "C++17"
-		staticruntime "On"
 		systemversion "latest"
+
 		defines
 		{
 			"HP_PLATFORM_WINDOWS"
@@ -121,15 +127,15 @@ project "Sandbox"
 
 	filter "configurations:Debug"
 		defines "HP_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
 		symbols "On"
 
 	filter "configurations:Release"
 		defines "HP_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
 
 	filter "configurations:Dist"
 		defines "HP_DIST"
-		buildoptions "/MD"
+		runtime "Release"
 		optimize "On"
