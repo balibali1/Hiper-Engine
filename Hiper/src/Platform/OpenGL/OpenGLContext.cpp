ed9fef452f053c695cfc6339473ae5de0776e372
yym <771339512@qq.com>
2024-04-21 16:49:55 +0800

    First Triangle！

--- Hiper/src/Platform/OpenGL/OpenGLContext.cpp
+++ Hiper/src/Platform/OpenGL/OpenGLContext.cpp
@@ -4,13 +4,14 @@
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GL/GL.h>
 
 namespace Hiper
 {
 	OpenGLContext::OpenGLContext(GLFWwindow* windowHandle)
 		:m_WindowHandle(windowHandle)
 	{
-		HP_CORE_ASSERT(windowHandle, "Window handle is null!");
+		HP_CORE_ASSERT(windowHandle, "Window handle is null!")
 	}
 
 	void OpenGLContext::Init()
@@ -18,6 +19,11 @@ namespace Hiper
 		glfwMakeContextCurrent(m_WindowHandle);
 		int state = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		HP_CORE_ASSERT(state, "Fail to initialize Glad!");
+
+		HP_CORE_INFO("OpenGL Info:");
+		HP_CORE_INFO("  Vendor: {0}", (const char*)glGetString(GL_VENDOR));
+		HP_CORE_INFO("  Renderer: {0}", (const char*)glGetString(GL_RENDERER));
+		HP_CORE_INFO("  Version: {0}", (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers()
